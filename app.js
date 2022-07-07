8a2f44d02de269835d893bcb9d42f642872a6154
JustYourBud <baconhatbuddy@gmail.com>
2022-07-07 09:51:21 -0500

    Update version numbers

--- app.js
+++ app.js
@@ -155,23 +155,6 @@ process.on("SIGINT", () => {
     console.log("\x1b[36m%s\x1b[0m", "SIGINT detected, exiting...")
     process.exit(0)
 })
-// check update repo
-const fetch = require("node-fetch")
+
 const { version } = require("./version.json")
 console.log("\x1b[33m%s\x1b[0m", `Current version : ${version}`)
-fetch("https://raw.githubusercontent.com/HELLSNAKES/Music-Slash-Bot/main/version.json")
-    .then((res) => res.json())
-    .then((data) => {
-        if (data.version !== version) {
-            console.log("\x1b[32m%s\x1b[0m", "===============================Update Available===================================")
-            console.log("Ver:", data.version)
-            console.log("\x1b[36m%s\x1b[0m", "Check commit : https://github.com/HELLSNAKES/Music-Slash-Bot/commits/main")
-            console.log("\x1b[31m%s\x1b[0m", "Use `npm run updatebot` to update")
-            console.log("\x1b[32m%s\x1b[0m", "==================================================================================")
-        } else {
-            console.log("\x1b[32m%s\x1b[0m", "No Update Available")
-        }
-    })
-    .catch((err) => {
-        console.log("\x1b[31m%s\x1b[0m", err)
-    })
